7097d572ee9535caf733f7143ee052095cbd3a21
GatzeStreicheln <login@gatze.ch>
2026-04-14 18:16:11 +0200

    Initial Commit

---
+++ Shelv Desktop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shelv Desktop
+//
+//  Created by Vasco Kugler on 14.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shelv Desktop/Shelv_DesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shelv_DesktopApp.swift
+//  Shelv Desktop
+//
+//  Created by Vasco Kugler on 14.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Shelv_DesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
